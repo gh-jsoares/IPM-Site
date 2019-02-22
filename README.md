2d2f28c90db5c66a9ed6f8cebe2d178c872a41e5
João Soares <jsoares.public@gmail.com>
2019-02-22 18:55:24 +0000

    Added pretty icons

--- README.md
+++ README.md
@@ -4,11 +4,11 @@ Pretty site for our stuff!
 
 ## Resources
  - **HTML5 Basics** playlist by *DevTips*
- 
- 
+
+
  [![HTML5 Basics](https://img.youtube.com/vi/NzzGt7EmXVw/0.jpg)](https://www.youtube.com/playlist?list=PLqGj3iMvMa4KlJn1pMYPVV3eYzxJlWcON)
- 
+
  - **CSS Basics** playlist by *DevTips*
- 
- 
+
+
  [![CSS Basics](https://img.youtube.com/vi/s7ONvIgOWdM/0.jpg)](https://www.youtube.com/playlist?list=PLqGj3iMvMa4IOmy04kDxh_hqODMqoeeCy&fbclid=IwAR2lfWgtG23IsrQZegbfohGNb9oBFCGSH5X4wSQeQPgka0DhRObGb_-QFo4)
--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,10 +1,28 @@
+const BASE_ICON     = 'icon'
+const CLOSE_ICON    = 'icon-up-open'
+const OPEN_ICON     = 'icon-down-open'
+
 function toggleSiblingVisible(elem) {
+    toggleIconChange(icon(elem))
     toggleVisible(sibling(elem))
 }
 
 function toggleVisible(elem) {
     elem.classList.toggle('hidden')
 }
+
+function toggleIconChange(elem) {
+    if (elem === null) return
+    const classes = elem.classList
+    if (classes.contains(CLOSE_ICON)) {
+        classes.remove(CLOSE_ICON)
+        classes.add(OPEN_ICON)
+    } else {
+        classes.remove(OPEN_ICON)
+        classes.add(CLOSE_ICON)
+    }
+}
+
 
 /**
  * nextSibling return a text element in firefox. This
@@ -18,8 +36,26 @@ function sibling(elem) {
     return elem
 }
 
-Array.forEach(document.getElementsByClassName('section-title'), element => {
+/**
+ * Returns the icon of the title. This icon represents
+ * if the tab pane is open or not.
+ */
+function icon(elem) {
+    elem = elem.getElementsByTagName('i')
+    elem = elem.length ? elem[0] : null
+    return elem
+}
+
+Array.from(document.getElementsByClassName('section-title')).forEach(element => {
     element.addEventListener('click', () => {
         toggleSiblingVisible(element)
     })
 })
+
+window.onload = () => {
+    Array.from(document.getElementsByClassName('icon')).forEach(element => {
+        const classes = element.classList
+        classes.remove(BASE_ICON)
+        classes.add(CLOSE_ICON)
+    })
+}
